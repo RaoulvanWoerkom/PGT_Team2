d10034d89f4c7440cbcec6795972924373373309
Sannz <sanjaykalloe@hotmail.com>
2017-03-13 15:10:52 +0100

    Terrain Generation..
    - Added temporary terrain texture

--- PGT2/Game/src/TestApplication.cpp
+++ PGT2/Game/src/TestApplication.cpp
@@ -1,6 +1,14 @@
 #include "TestApplication.h"
 
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
+
+#include "BaseApplication.h"
+
 TestApplication::TestApplication(void)
+	: mTerrainGroup(0),
+	mTerrainGlobals(0),
+	mInfoLabel(0)
 {
 }
 
@@ -17,6 +25,17 @@ void TestApplication::createCamera()
 	mCamera->setNearClipDistance(5);
 
 	mCameraMan = new OgreBites::SdkCameraMan(mCamera);
+
+	bool infiniteClip =
+		mRoot->getRenderSystem()->getCapabilities()->hasCapability(
+		Ogre::RSC_INFINITE_FAR_PLANE);
+
+	if (infiniteClip)
+		mCamera->setFarClipDistance(0);
+	else
+		//mCamera->setFarClipDistance(932500000);
+		mCamera->setFarClipDistance(1390000);
+
 }
 
 void TestApplication::createViewports()
@@ -31,24 +50,181 @@ void TestApplication::createViewports()
 
 }
 
+
+
+//Classes for Terraingeneration
+void TestApplication::createFrameListener()
+{
+	BaseApplication::createFrameListener();
+	mInfoLabel = mTrayMgr->createLabel(OgreBites::TL_TOP, "TerrainInfo", "", 350);
+}
+
+void TestApplication::destroyScene()
+{
+	OGRE_DELETE mTerrainGroup;
+	OGRE_DELETE mTerrainGlobals;
+}
+
+bool TestApplication::frameRenderingQueued(const Ogre::FrameEvent& fe)
+{
+	bool ret = BaseApplication::frameRenderingQueued(fe);
+
+	if (mTerrainGroup->isDerivedDataUpdateInProgress())
+	{
+		mTrayMgr->moveWidgetToTray(mInfoLabel, OgreBites::TL_TOP, 0);
+		mInfoLabel->show();
+
+		if (mTerrainsImported)
+			mInfoLabel->setCaption("Building terrain...");
+		else
+			mInfoLabel->setCaption("Updating terrain...");
+	}
+	else
+	{
+		mTrayMgr->removeWidgetFromTray(mInfoLabel);
+		mInfoLabel->hide();
+
+		if (mTerrainsImported)
+		{
+			mTerrainGroup->saveAllTerrains(true);
+			mTerrainsImported = false;
+		}
+	}
+
+	return ret;
+}
+
+void getTerrainImage(bool flipX, bool flipY, Ogre::Image& img)
+{
+	img.load("terrain.png", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+
+	if (flipX)
+		img.flipAroundY();
+	if (flipY)
+		img.flipAroundX();
+}
+
+void TestApplication::defineTerrain(long x, long y)
+{
+	Ogre::String filename = mTerrainGroup->generateFilename(x, y);
+
+	bool exists =
+		Ogre::ResourceGroupManager::getSingleton().resourceExists(
+		mTerrainGroup->getResourceGroup(),
+		filename);
+
+	if (exists)
+		mTerrainGroup->defineTerrain(x, y);
+	else
+	{
+		Ogre::Image img;
+		getTerrainImage(x % 2 != 0, y % 2 != 0, img);
+		mTerrainGroup->defineTerrain(x, y, &img);
+
+		mTerrainsImported = true;
+	}
+
+
+}
+
+void TestApplication::initBlendMaps(Ogre::Terrain* terrain)
+{
+	Ogre::Real minHeight0 = 70;
+	Ogre::Real fadeDist0 = 40;
+	Ogre::Real minHeight1 = 70;
+	Ogre::Real fadeDist1 = 15;
+
+	Ogre::TerrainLayerBlendMap* blendMap0 = terrain->getLayerBlendMap(1);
+	Ogre::TerrainLayerBlendMap* blendMap1 = terrain->getLayerBlendMap(2);
+
+	float* pBlend0 = blendMap0->getBlendPointer();
+	float* pBlend1 = blendMap1->getBlendPointer();
+
+	for (Ogre::uint16 y = 0; y < terrain->getLayerBlendMapSize(); ++y)
+	{
+		for (Ogre::uint16 x = 0; x < terrain->getLayerBlendMapSize(); ++x)
+		{
+			Ogre::Real tx, ty;
+
+			blendMap0->convertImageToTerrainSpace(x, y, &tx, &ty);
+			Ogre::Real height = terrain->getHeightAtTerrainPosition(tx, ty);
+			Ogre::Real val = (height - minHeight0) / fadeDist0;
+			val = Ogre::Math::Clamp(val, (Ogre::Real)0, (Ogre::Real)1);
+			*pBlend0++ = val;
+
+			val = (height - minHeight1) / fadeDist1;
+			val = Ogre::Math::Clamp(val, (Ogre::Real)0, (Ogre::Real)1);
+			*pBlend1++ = val;
+		}
+	}
+
+	blendMap0->dirty();
+	blendMap1->dirty();
+	blendMap0->update();
+	blendMap1->update();
+}
+
+void TestApplication::configureTerrainDefaults(Ogre::Light* light)
+{
+	mTerrainGlobals->setMaxPixelError(8);
+	mTerrainGlobals->setCompositeMapDistance(3000);
+
+	mTerrainGlobals->setLightMapDirection(light->getDerivedDirection());
+	mTerrainGlobals->setCompositeMapAmbient(mSceneMgr->getAmbientLight());
+	mTerrainGlobals->setCompositeMapDiffuse(light->getDiffuseColour());
+
+	Ogre::Terrain::ImportData& importData = mTerrainGroup->getDefaultImportSettings();
+	importData.terrainSize = 5130;
+	//importData.worldSize = 12000.0;
+	importData.worldSize = 12000.0;
+	importData.inputScale = 600;
+	//importData.minBatchSize = 33;
+	//importData.maxBatchSize = 65;
+	importData.minBatchSize = 3;
+	importData.maxBatchSize = 65;
+
+	//How many different texture will it have
+	importData.layerList.resize(3);
+
+	//Adding texture.
+	importData.layerList[0].worldSize = 100;
+	importData.layerList[0].textureNames.push_back(
+		"dirt_grayrocky_diffusespecular.dds");
+	importData.layerList[0].textureNames.push_back(
+		"dirt_grayrocky_normalheight.dds");
+	importData.layerList[1].worldSize = 30;
+	importData.layerList[1].textureNames.push_back(
+		"grass_green-01_diffusespecular.dds");
+	importData.layerList[1].textureNames.push_back(
+		"grass_green-01_normalheight.dds");
+	importData.layerList[2].worldSize = 200;
+	importData.layerList[2].textureNames.push_back(
+		"growth_weirdfungus-03_diffusespecular.dds");
+	importData.layerList[2].textureNames.push_back(
+		"growth_weirdfungus-03_normalheight.dds");
+}
+
 void TestApplication::createScene()
 {
 	mSceneMgr->setAmbientLight(Ogre::ColourValue(0, 0, 0));
 	mSceneMgr->setShadowTechnique(Ogre::SHADOWTYPE_STENCIL_ADDITIVE);
 
 	// Create ground
-	Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
+	//	Ogre::Plane plane(Ogre::Vector3::UNIT_Y, 0);
 
-	Ogre::MeshManager::getSingleton().createPlane(
-		"ground",
-		Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
-		plane, 1500, 1500, 20, 20, true, 1, 5, 5, Ogre::Vector3::UNIT_Z);
+	//	Ogre::MeshManager::getSingleton().createPlane(
+	//		"ground",
+	//		Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+	//		plane, 1500, 1500, 20, 20, true, 1, 5, 5, Ogre::Vector3::UNIT_Z);
 
-	Ogre::Entity* groundEntity = mSceneMgr->createEntity("ground");
-	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
+	//	Ogre::Entity* groundEntity = mSceneMgr->createEntity("ground");
+	//	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
 
-	groundEntity->setMaterialName("Examples/Rockwall");
-	groundEntity->setCastShadows(false);
+	//	groundEntity->setMaterialName("Examples/Rockwall");
+	//	groundEntity->setCastShadows(false); 
+
+	//Create environment
+	mSceneMgr->setSkyBox(true, "Examples/CloudyNoonSkyBox");
 
 	// Directional light
 	Ogre::Light* directionalLight = mSceneMgr->createLight("DirectionalLight");
@@ -59,6 +235,52 @@ void TestApplication::createScene()
 
 	directionalLight->setDirection(Ogre::Vector3(0, -1, 1));
 
+	//Foggy distance
+	//Ogre::ColourValue fadeColour(0.9, 0.9, 0.9);
+	//mWindow->getViewport(0)->setBackgroundColour(fadeColour);
+	//mSceneMgr->setFog(Ogre::FOG_LINEAR, fadeColour, 0, 2600, 2900);
+	//mSceneMgr->setFog(Ogre::FOG_EXP, fadeColour, 0.0002);
+
+
+
+	//Create Terrain
+	mTerrainGlobals = OGRE_NEW Ogre::TerrainGlobalOptions();
+
+	//mTerrainGroup = OGRE_NEW Ogre::TerrainGroup(
+		//mSceneMgr,
+		//Ogre::Terrain::ALIGN_X_Z,
+		//513, 12000.0);
+	mTerrainGroup = OGRE_NEW Ogre::TerrainGroup(
+		mSceneMgr,
+		Ogre::Terrain::ALIGN_X_Z,
+		65, 32000.0);
+
+
+	mTerrainGroup->setFilenameConvention(Ogre::String("terrain"), Ogre::String("dat"));
+	mTerrainGroup->setOrigin(Ogre::Vector3::ZERO);
+
+	
+
+	configureTerrainDefaults(directionalLight);
+
+	for (long x = 0; x <= 0; ++x)
+		for (long y = 0; y <= 0; ++y)
+			defineTerrain(x, y);
+
+	mTerrainGroup->loadAllTerrains(true);
+
+	if (mTerrainsImported)
+	{
+		Ogre::TerrainGroup::TerrainIterator ti = mTerrainGroup->getTerrainIterator();
+
+		while (ti.hasMoreElements())
+		{
+			Ogre::Terrain* t = ti.getNext()->instance;
+			initBlendMaps(t);
+		}
+	}
+
+	mTerrainGroup->freeTemporaryResources();
 }
 
 
--- PGT2/Game/src/TestApplication.h
+++ PGT2/Game/src/TestApplication.h
@@ -3,6 +3,9 @@
 
 #include "BaseApplication.h"
 
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
+
 class TestApplication : public BaseApplication
 {
 public:
@@ -13,6 +16,21 @@ protected:
 	virtual void createScene();
 	virtual void createCamera();
 	virtual void createViewports();
+
+	virtual void createFrameListener();
+	virtual void destroyScene();
+	virtual bool frameRenderingQueued(const Ogre::FrameEvent& fe);
+
+private:
+	void defineTerrain(long x, long y);
+	void initBlendMaps(Ogre::Terrain* terrain);
+	void configureTerrainDefaults(Ogre::Light* light);
+
+	bool mTerrainsImported;
+	Ogre::TerrainGroup* mTerrainGroup;
+	Ogre::TerrainGlobalOptions* mTerrainGlobals;
+	OgreBites::Label* mInfoLabel;
+
 };
 
 #endif // #ifndef __TestApplication_h_
